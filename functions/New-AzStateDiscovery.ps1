d7a9e02b5a77bea0bf08e5ffa3629038cf3845a5
Kevin Rowlandson <kevin.rowlandson@microsoft.com>
2020-09-03 08:25:35 +0100

    Suppress PSReviewUnusedParameter false positive

--- functions/New-AzStateDiscovery.ps1
+++ functions/New-AzStateDiscovery.ps1
@@ -3,6 +3,14 @@
 #################################
 
 function Get-AzStateChildrenByType {
+    ###############################################
+    # Configure PSScriptAnalyzer rule suppression #
+    ###############################################
+
+    # The following SuppressMessageAttribute entries are used to surpress
+    # PSScriptAnalyzer tests against known exceptions as per:
+    # https://github.com/powershell/psscriptanalyzer#suppressing-rules
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', 'ExcludePathIds', Justification = 'False positive: used in process block of function')]
 
     [CmdletBinding()]
     [OutputType([AzState[]])]
